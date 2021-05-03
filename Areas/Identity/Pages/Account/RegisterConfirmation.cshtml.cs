0fcad765ebdca41039c605e249e72716e0624fa3
Osmay Cruz <osmay.cruz@gmail.com>
2021-05-03 10:06:20 -0400

    app runs fine on docker

---
+++ Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Authorization;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace proyecto.Areas.Identity.Pages.Account
+{
+[AllowAnonymous]
+  public class RegisterConfirmationModel : PageModel
+  {
+      public IActionResult OnGet()
+      {  
+          return Page();
+      }
+  }
+}
